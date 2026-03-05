a1801d3b4a3f31d6da22c47ef3e2bb7dff627aae
aniellyvaleria <aniellyvaleriaalmeida@gmail.com>
2026-03-05 12:52:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("nome").innerText = "Anielly Valéria Almeida";
+
+document.getElementById("info").innerHTML = `
+14/07/2010 <br>
+(81) 99943-1220 <br>
+avmna@discente.ifpe.edu.br`
+
+function github() {
+    window.open("https://github.com/aniellyvaleria")
+}
+function IT7() {
+    window.open("IT7anielly.pdf")
+}
